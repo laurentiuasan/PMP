9f68dfeb512071f22f80be44c2655f4f849bd5a2
laurentiuasan <lau.lau95@yahoo.com>
2022-09-30 13:23:34 +0300

    Added Gaussian Distribution (X3).

--- Lab1/hello_world.py
+++ Lab1/hello_world.py
@@ -5,11 +5,10 @@ import numpy as np
 
 import pymc3 as pm
 
-
 # Generating data
 # True parameter values
 alpha, sigma = 1, 1
-beta = [1, 2.5]
+beta = [1, 2.5, 0.1]
 
 # Size of dataset
 size = 100
@@ -17,35 +16,39 @@ size = 100
 # Predictor variable
 X1 = np.random.randn(size)
 X2 = np.random.randn(size) * 0.2
+X3 = np.random.randn(size)
+
 
 # Simulate outcome variable
-Y = alpha + beta[0] * X1 + beta[1] * X2 + np.random.randn(size) * sigma
+# Y = alpha + beta[0] * X1 + beta[1] * X2 + beta[2] * X3 + np.random.randn(size) * sigma
+Y = alpha + (np.pi*np.std(X3)) * np.exp(-0.5*((X3 - np.mean(X3))/np.std(X3))**2) + np.random.rand(size) * sigma
 
 # Vizualize the data
-fig, axes = plt.subplots(1, 2, sharex=True, figsize=(10, 4))
+fig, axes = plt.subplots(1, 3, sharex=True, figsize=(10, 4))
 axes[0].scatter(X1, Y, alpha=0.6)
 axes[1].scatter(X2, Y, alpha=0.6)
+axes[2].scatter(X3, Y, alpha=0.6)
 axes[0].set_ylabel("Y")
 axes[0].set_xlabel("X1")
 axes[1].set_xlabel("X2")
+axes[2].set_xlabel("X3")
 plt.show()
 
 # Define a model
 basic_model = pm.Model()
 
 with basic_model:
-
     # Priors for unknown model parameters
     alpha = pm.Normal("alpha", mu=0, sigma=10)
     beta = pm.Normal("beta", mu=0, sigma=10, shape=2)
     sigma = pm.HalfNormal("sigma", sigma=1)
 
     # Expected value of outcome
-    mu = alpha + beta[0] * X1 + beta[1] * X2
+    mu = alpha + beta[0] * X1 + beta[1] * X2 + beta[2] * X3
 
     # Likelihood (sampling distribution) of observations
     Y_obs = pm.Normal("Y_obs", mu=mu, sigma=sigma, observed=Y)
 
-#Fit model to data and print estimates
+# Fit model to data and print estimates
 map_estimate = pm.find_MAP(model=basic_model)
 print(map_estimate)
